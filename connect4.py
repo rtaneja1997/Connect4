69456654067871fb7fe95ca12a5f12f66efcc4c8
lqw5 <lqw5@cornell.edu>
2018-12-05 11:51:17 -0500

    Fixed end game bug. Set up automatted play

--- connect4.py
+++ connect4.py
@@ -19,7 +19,7 @@ width = COLUMN_COUNT * SQUARESIZE
 height = (ROW_COUNT+1) * SQUARESIZE
 
 def create_board():
-	#board = np.zeros([ROW_COUNT, COLUMN_COUNT]) 
+	#board = np.zeros([ROW_COUNT, COLUMN_COUNT])
 	board=[[0 for i in range(COLUMN_COUNT)] for j in range(ROW_COUNT)]
 	return board
 
@@ -35,9 +35,9 @@ def get_next_open_row(board, col):
 		if board[r][col] == 0:
 			return r
 
-def print_board(board):
-	#print (np.flip(board)) 
-	print ("ignore")
+#def print_board(board):
+	#print (np.flip(board))
+	#print ("ignore")
 
 def winning_move(board, piece):
 	# Check horizontal locations for win
@@ -69,21 +69,23 @@ def draw_board(board, screen):
 		for r in range(ROW_COUNT):
 			pygame.draw.rect(screen, BLUE, (c*SQUARESIZE, r*SQUARESIZE+SQUARESIZE, SQUARESIZE, SQUARESIZE))
 			pygame.draw.circle(screen, BLACK, (int(c*SQUARESIZE+SQUARESIZE/2), int(r*SQUARESIZE+SQUARESIZE+SQUARESIZE/2)), RADIUS)
-	
+
 	for c in range(COLUMN_COUNT):
-		for r in range(ROW_COUNT):		
+		for r in range(ROW_COUNT):
 			if board[r][c] == 1:
 				pygame.draw.circle(screen, RED, (int(c*SQUARESIZE+SQUARESIZE/2), height-int(r*SQUARESIZE+SQUARESIZE/2)), RADIUS)
-			elif board[r][c] == 2: 
+			elif board[r][c] == 2:
 				pygame.draw.circle(screen, YELLOW, (int(c*SQUARESIZE+SQUARESIZE/2), height-int(r*SQUARESIZE+SQUARESIZE/2)), RADIUS)
 	pygame.display.update()
 
 def play_game(mode):
 	mode = mode;
 	board = create_board()
-	print_board(board)
+	#print_board(board)
 	game_over = False
+	to_game_over = False
 	turn = 0
+	winner = -1
 
 	pygame.init()
 
@@ -93,88 +95,202 @@ def play_game(mode):
 	draw_board(board, screen)
 	pygame.display.update()
 
-	myfont = pygame.font.SysFont("monospace", 75)
+	myfont = pygame.font.SysFont("monospace", 50)
+	if( mode == "auto_ai_vs_ai" or mode == "auto_ai_vs_random" or mode == "auto_random_vs_ai"):
+		while not game_over:
+			if turn == 0:
+				# if (mode == "human_vs_random" or mode == "human_vs_ai"):
+				# 	posx = event.pos[0]
+				# 	col = int(math.floor(posx/SQUARESIZE))
+				#
+				# 	if is_valid_location(board, col):
+				# 		row = get_next_open_row(board, col)
+				# 		drop_piece(board, row, col, 1)
 
-	while not game_over:
-
-		for event in pygame.event.get():
-			if event.type == pygame.QUIT:
-				sys.exit()
-
-			if event.type == pygame.MOUSEMOTION:
-				pygame.draw.rect(screen, BLACK, (0,0, width, SQUARESIZE))
-				posx = event.pos[0]
-				if turn == 0:
-					pygame.draw.circle(screen, RED, (posx, int(SQUARESIZE/2)), RADIUS)
-				else: 
-					pygame.draw.circle(screen, YELLOW, (posx, int(SQUARESIZE/2)), RADIUS)
-			pygame.display.update()
-
-			if event.type == pygame.MOUSEBUTTONDOWN:
-				pygame.draw.rect(screen, BLACK, (0,0, width, SQUARESIZE))
-				#print(event.pos)
-				# Ask for Player 1 Input
-				if turn == 0:
-					posx = event.pos[0]
-					col = int(math.floor(posx/SQUARESIZE))
-
+				if (mode == "auto_random_vs_ai"):
+					col = play_random(COLUMN_COUNT)
 					if is_valid_location(board, col):
 						row = get_next_open_row(board, col)
 						drop_piece(board, row, col, 1)
-
-						if winning_move(board, 1):
-							label = myfont.render("Player 1 wins!!", 1, RED)
-							screen.blit(label, (40,10))
-							game_over = True
-
-
-				# # Ask for Player 2 Input
 				else:
-					if (mode == "human"):				
-						posx = event.pos[0]
-						col = int(math.floor(posx/SQUARESIZE))
+					col=play_smart(board)
+					row=get_next_open_row(board, col)
+					drop_piece(board,row,col,1)
 
-						if is_valid_location(board, col):
-							row = get_next_open_row(board, col)
-							drop_piece(board, row, col, 2)
+				if winning_move(board, 1):
+					game_over = True
+					print('player 1 win')
+					label = myfont.render("Player 1 wins!! Press any key to exit", 1, RED)
+					#time.sleep(1)
+					screen.blit(label, (40,10))
+					winner = 1
 
-							if winning_move(board, 2):
-								label = myfont.render("Player 2 wins!!", 1, YELLOW)
-								screen.blit(label, (40,10))
-								game_over = True
 
-						turn += 1
-						turn = turn % 2
 
-				if (mode == "random"):
-					
+			# # Ask for Player 2 Input
+			else:
+				# if (mode == "human"):
+				# 	posx = event.pos[0]
+				# 	col = int(math.floor(posx/SQUARESIZE))
+				#
+				# 	if is_valid_location(board, col):
+				# 		row = get_next_open_row(board, col)
+				# 		drop_piece(board, row, col, 2)
+				#
+				# 		if winning_move(board, 2):
+				# 			label = myfont.render("Player 2 wins!! Press any key to exit", 1, YELLOW)
+				# 			#time.sleep(1)
+				# 			screen.blit(label, (40,10))
+				# 			winner = 2
+				# 			to_game_over = True
+
+				turn += 1
+				turn = turn % 2
+			if( not game_over):
+				if (mode == "auto_ai_vs_random"):
+
 					col = play_random(COLUMN_COUNT)
 					if is_valid_location(board, col):
 						row = get_next_open_row(board, col)
 						drop_piece(board, row, col, 2)
-        
+
 					if winning_move(board, 2):
 						game_over = True #ai stuff
-						label = myfont.render("Player 2 wins!!", 1, YELLOW)
+						label = myfont.render("Player 2 wins!! Press any key to exit", 1, YELLOW)
+						#time.sleep(1)
 						screen.blit(label, (40,10))
+						winner = 2
 
-								
-				if (mode == "minimax"):
+				if (mode == "auto_ai_vs_ai" or mode == "auto_random_vs_ai"):
 
-					#make move 
-					col=play_smart(board) 
-					row=get_next_open_row(board, col) 
-					drop_piece(board,row,col,2) 
+					#make move
+					col=play_smart(board)
+					row=get_next_open_row(board, col)
+					drop_piece(board,row,col,2)
 
 					#check for win
-					if winning_move(board,2): 
-						game_over=True 
-						label=myfont.render("Player 2 wins!!", 1, YELLOW) 
-						screen.blit(label, (40,10)) 
+					if winning_move(board,2):
+						game_over = True
+						label = myfont.render("Player 2 wins!! Press any key to exit.", 1, YELLOW)
+						screen.blit(label, (40,10))
+
+						winner = 2
+
+			#print_board(board)
+			draw_board(board, screen)
 
 
-				print_board(board)
-				draw_board(board, screen)
+	else:
+		while not game_over:
 
-				if game_over:
-					pygame.time.wait(3000)	
+			for event in pygame.event.get():
+				if event.type == pygame.QUIT:
+					sys.exit()
+
+				if event.type == pygame.MOUSEMOTION:
+					pygame.draw.rect(screen, BLACK, (0,0, width, SQUARESIZE))
+					posx = event.pos[0]
+					if turn == 0:
+						pygame.draw.circle(screen, RED, (posx, int(SQUARESIZE/2)), RADIUS)
+					else:
+						pygame.draw.circle(screen, YELLOW, (posx, int(SQUARESIZE/2)), RADIUS)
+				pygame.display.update()
+
+				if event.type == pygame.MOUSEBUTTONDOWN:
+					pygame.draw.rect(screen, BLACK, (0,0, width, SQUARESIZE))
+					#print(event.pos)
+					# Ask for Player 1 Input
+					if turn == 0:
+						if (mode == "human_vs_random" or mode == "human_vs_ai"):
+							posx = event.pos[0]
+							col = int(math.floor(posx/SQUARESIZE))
+
+							if is_valid_location(board, col):
+								row = get_next_open_row(board, col)
+								drop_piece(board, row, col, 1)
+
+						elif (mode == "random_vs_ai"):
+							col = play_random(COLUMN_COUNT)
+							if is_valid_location(board, col):
+								row = get_next_open_row(board, col)
+								drop_piece(board, row, col, 1)
+						else:
+							col=play_smart(board)
+							row=get_next_open_row(board, col)
+							drop_piece(board,row,col,1)
+
+						if winning_move(board, 1):
+							print('player 1 win')
+							label = myfont.render("Player 1 wins!! Press any key to exit", 1, RED)
+							#time.sleep(1)
+							screen.blit(label, (40,10))
+							winner = 1
+							to_game_over = True
+
+
+					# # Ask for Player 2 Input
+					else:
+						if (mode == "human"):
+							posx = event.pos[0]
+							col = int(math.floor(posx/SQUARESIZE))
+
+							if is_valid_location(board, col):
+								row = get_next_open_row(board, col)
+								drop_piece(board, row, col, 2)
+
+								if winning_move(board, 2):
+									label = myfont.render("Player 2 wins!! Press any key to exit", 1, YELLOW)
+									#time.sleep(1)
+									screen.blit(label, (40,10))
+									winner = 2
+									to_game_over = True
+
+							turn += 1
+							turn = turn % 2
+					if( not to_game_over):
+						if (mode == "human_vs_random" or mode == "ai_vs_random"):
+
+							col = play_random(COLUMN_COUNT)
+							if is_valid_location(board, col):
+								row = get_next_open_row(board, col)
+								drop_piece(board, row, col, 2)
+
+							if winning_move(board, 2):
+								to_game_over = True #ai stuff
+								label = myfont.render("Player 2 wins!! Press any key to exit", 1, YELLOW)
+								#time.sleep(1)
+								screen.blit(label, (40,10))
+								winner = 2
+
+						if (mode == "human_vs_ai" or mode == "ai_vs_ai" or mode == "random_vs_ai"):
+
+							#make move
+							col=play_smart(board)
+							row=get_next_open_row(board, col)
+							drop_piece(board,row,col,2)
+
+							#check for win
+							if winning_move(board,2):
+								to_game_over = True
+								label = myfont.render("Player 2 wins!! Press any key to exit.", 1, YELLOW)
+								screen.blit(label, (40,10))
+
+								winner = 2
+
+					#print_board(board)
+					draw_board(board, screen)
+
+					if to_game_over:
+						print("over")
+						waitExit = True
+						while(waitExit):
+							for event in pygame.event.get():
+								if (event.type == pygame.KEYDOWN or event.type == pygame.QUIT):
+									waitExit = False
+						game_over = True
+						#label=myfont.render("Player 2 wins!!", 1, YELLOW)
+
+						#screen.blit(label, (40,10))
+
+						#pygame.time.wait(3000)
+	return winner
--- main.py
+++ main.py
@@ -1,10 +1,70 @@
-#runs the game 
+#runs the game
 
 import numpy as np
 import pygame
 import sys
 import math
-import connect4 
-from connect4 import * 
+import connect4
+from connect4 import *
 
-play_game("minimax")
+def displayText(screen):
+    myfont = pygame.font.SysFont("monospace", 30)
+    text = ["Welcome to Connect 4!","Press 1 for player vs. random", "Press 2 for player vs. AI"
+    ,"Press 3 for AI vs. random", "Press 4 for random vs. AI","Press 5 for AI vs. AI",
+    "Press 6 for Automated AI vs. Random", "Press 7 for Automated Random vs. AI",
+    "Press 8 for Automated AI vs. AI"]
+    label = []
+    for line in text:
+        label.append(myfont.render(line, 1, (255,255,255)))
+    for line in range(len(label)):
+        screen.blit(label[line], (40,10 +(line*30+(15*line))))
+    pygame.display.update()
+
+def main():
+    SQUARESIZE = 100
+    RADIUS = int(SQUARESIZE/2 - 5)
+    ROW_COUNT = 6
+    COLUMN_COUNT = 7
+    width = COLUMN_COUNT * SQUARESIZE
+    height = (ROW_COUNT+1) * SQUARESIZE
+    size = (width, height)
+    pygame.init()
+    screen = pygame.display.set_mode(size)
+    displayText(screen)
+    pygame.display.update()
+
+    waitStart =True
+    while(waitStart):
+        for event in pygame.event.get():
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_1:
+                    waitStart = False
+                    print(play_game("human_vs_random"))
+                elif event.key == pygame.K_2:
+                    print(play_game("human_vs_ai"))
+                    waitStart = False
+                elif event.key == pygame.K_3:
+                    print(play_game("ai_vs_random"))
+                    waitStart = False
+                elif event.key == pygame.K_4:
+                    print(play_game("random_vs_ai"))
+                    waitStart = False
+                elif event.key == pygame.K_5:
+                    print(play_game("ai_vs_ai"))
+                    waitStart = False
+                elif event.key == pygame.K_6:
+                    for i in range(10):
+                        print(play_game("auto_ai_vs_random"))
+                    waitStart = False
+                elif event.key == pygame.K_7:
+                    for i in range(10):
+                        print(play_game("auto_random_vs_ai"))
+                    waitStart = False
+                elif event.key == pygame.K_8:
+                    for i in range(10):
+                        print(play_game("auto_ai_vs_ai"))
+                    waitStart = False
+            elif event.type == pygame.QUIT:
+                sys.exit()
+
+if __name__ == "__main__": main()
